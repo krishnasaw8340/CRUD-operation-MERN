26cf0cbbae8db01d282078dc8b8f141e8aceb290
Krishna Saw <krishnakrsaw8340@gmail.com>
2024-02-02 20:34:34 +0530

    changes

--- server/Delete/delete.js
+++ server/Delete/delete.js
@@ -1,12 +1,12 @@
 const PhoneBook = require("../db/db")
 const deleteOne = async(req,res) =>{
-    await PhoneBook.findByIdAndDelete(req.param.id)
+    await PhoneBook.findByIdAndDelete(req.params.id)
     try{
         res.status(204).json({
             status : 'Success',
+            message: "Deleted",
             data : {}
         })
-
     }
     catch(err) {
         res.status(500).json({
